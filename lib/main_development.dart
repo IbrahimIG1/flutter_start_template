50d51d0488cf5fedf5ba37d04f1f12cc6af679db
IbrahimIG1 <ibrahim.algammal.info@gmail.com>
2025-11-11 14:10:13 +0200

    test

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -60,7 +60,7 @@ class MyApp extends StatelessWidget {
                   initialRoute:
                       token.isNullOrEmpty()
                           ? ScreensNames.layoutScreen
-                          : ScreensNames.layoutScreen,
+                          : ScreensNames.layoutScreen, 
                 );
               },
             ),
